51946db11df99f9f592a4fc004a19965d2e88b02
Rounak Kumar Singh <rounak.official.2@gmail.com>
2025-12-05 23:30:50 +0530

    added sprites

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
+
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    p = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        p.update(dt)
         screen.fill("black")
+        p.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
